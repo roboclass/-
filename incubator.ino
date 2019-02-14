d70f38bcdc6704736ada69540daa5959984d9424
Vinaria <47085257+Vinaria@users.noreply.github.com>
2019-02-14 17:10:07 +0300

    Add files via upload
    Вторая рабочая программа (есть транзистор, светодиод, обновляется влажность)

---
+++ incubator.ino
@@ -0,0 +1,90 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Senso
+
+#include "DHT.h"
+#define red 3
+#define green 6
+#define yellow 5
+#define waterheat 2
+#define DHTPIN 7    // what digital pin we're connected to
+#include <LiquidCrystal.h>
+int backlight=3;
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT22   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(backlight, OUTPUT);
+  pinMode(waterheat,OUTPUT);
+  digitalWrite(backlight, HIGH);
+  lcd.begin(16, 2);
+  lcd.print("Hello word!");
+  delay(5000);
+  lcd.clear();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  lcd.setCursor(0, 0);
+  lcd.print(F("Hum: "));
+  lcd.print(h);
+  lcd.print("%");
+  lcd.setCursor(0, 1);
+  lcd.print(F("Temp: "));
+  lcd.print(t);
+  lcd.print("\xdf");
+  lcd.print("C");
+  lcd.setCursor(0, 1);
+  
+  Serial.print("Hum: ");
+  Serial.print(h);
+  Serial.println(" %");
+  Serial.print("Temp: ");
+  Serial.print(t);
+  Serial.println(" C");
+  Serial.println(" ");
+
+  if (abs(h - 65) >= 15){
+    digitalWrite(waterheat, HIGH);
+  }
+  else {
+    digitalWrite(waterheat, LOW);
+  }
+  }
